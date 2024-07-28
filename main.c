84312dbe2495451d28de0220ca14148b032e5594
NathanMathematician <nathandampied@gmail.com>
2024-07-28 08:23:57 +0200

    Add the possibility to plot as many function as wanted.

--- main.c
+++ main.c
@@ -9,7 +9,17 @@ int main(int argc, char* argv[])
 {
     printf("\n\nWelcome to ComplexMapping\n\n");
 
-    drawFunction();
+    unsigned char continueComplexMapping = 1;
+
+    while (continueComplexMapping == 1)
+    {
+        drawFunction();
+
+        printf("\n\nDo you want to plot another function?\t1.Yes\t2.No");
+        char *continueText = calloc(sizeof(char), INPUT_TEXT_MAX_CHAR_SIZE);
+        readKeyboardInput(continueText);
+        continueComplexMapping = strtod(continueText, NULL);
+    }
 
     return 0;
 }
